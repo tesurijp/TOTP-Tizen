546018fccc1dfdb48e06e4b39c4c64b6ef30bca0
tesuri <tesuri@gmail.com>
2018-11-23 14:26:16 +0900

    indent整理

--- js/main.js
+++ js/main.js
@@ -10,115 +10,115 @@ var tokenList = document.getElementById("Tokens");
 window.onload = Redraw();
 
 function calc(key, keytime, keylength) {
-  try{
-    var time = ("0000000000000000" + keytime.toString(16)).slice(-16);
-    var hmac = (new jsSHA(time,"HEX")).getHMAC(key, "HEX", "SHA-1", "HEX");
+    try{
+        var time = ("0000000000000000" + keytime.toString(16)).slice(-16);
+        var hmac = (new jsSHA(time,"HEX")).getHMAC(key, "HEX", "SHA-1", "HEX");
 
-    var offset = parseInt(hmac.substring(hmac.length - 1),16);
-    var otp = (parseInt(hmac.substr(offset * 2, 8),16) & parseInt("7fffffff",16)).toString();
+        var offset = parseInt(hmac.substring(hmac.length - 1),16);
+        var otp = (parseInt(hmac.substr(offset * 2, 8),16) & parseInt("7fffffff",16)).toString();
 
-    var spaces = new RegExp('.{' + keylength/2 + '}$');
+        var spaces = new RegExp('.{' + keylength/2 + '}$');
 
-    return otp.slice(keylength*-1).replace(spaces, ' $&');
-  }catch(err){
-    return "000 000";
-  }
+        return otp.slice(keylength*-1).replace(spaces, ' $&');
+    }catch(err){
+        return "000 000";
+    }
 }
 
 
 function UpdateRemainTime(nextTime){
-  var currentTime = Math.round(new Date().getTime() / 1000.0);
-  var calc_time = Math.floor(currentTime / 30);
-  var nextTime = 30 - (currentTime % 30);
-  UpdateRemainTimeInner(nextTime);
-  return calc_time;
+    var currentTime = Math.round(new Date().getTime() / 1000.0);
+    var calc_time = Math.floor(currentTime / 30);
+    var nextTime = 30 - (currentTime % 30);
+    UpdateRemainTimeInner(nextTime);
+    return calc_time;
 }
 function UpdateRemainTimeRectangle(nextTime){
-  remainTime.innerHTML = nextTime + REMAIN;
+    remainTime.innerHTML = nextTime + REMAIN;
 }
 function UpdateRemainTimeCircle(nextTime){
-  progressBarWidget.value(nextTime);
+    progressBarWidget.value(nextTime);
 }
 
 
 function InitList() {
-  var calc_time = UpdateRemainTime();
-  for(var i=0; i<keyItems.length; i++){
-    var item = keyItems[i];
-    var token = calc(item.key,calc_time,item.digits );
-    var item_id = "item_"+i;
-    var newelm = MakeLiItem(item_id, token,item.service, item.id);
-    tokenList.appendChild(newelm);
-  }
+    var calc_time = UpdateRemainTime();
+    for(var i=0; i<keyItems.length; i++){
+        var item = keyItems[i];
+        var token = calc(item.key,calc_time,item.digits );
+        var item_id = "item_"+i;
+        var newelm = MakeLiItem(item_id, token,item.service, item.id);
+        tokenList.appendChild(newelm);
+    }
 }
 
 function Update() {
-  var calc_time = UpdateRemainTime();
-  for(var i=0; i<keyItems.length; i++){
-    var item = keyItems[i];
-    var token = calc(item.key,calc_time,item.digits );
-    var item_id = "item_"+i;
+    var calc_time = UpdateRemainTime();
+    for(var i=0; i<keyItems.length; i++){
+        var item = keyItems[i];
+        var token = calc(item.key,calc_time,item.digits );
+        var item_id = "item_"+i;
 
-    var oldelm =  document.getElementById(item_id);
-    oldelm.childNodes[0].textContent = token;
-  }
+        var oldelm =  document.getElementById(item_id);
+        oldelm.childNodes[0].textContent = token;
+    }
 }
 
 function MakeLiItem (elem_id , token , service, id){
-  var li = document.createElement("li");
-  li.setAttribute("class","li-has-multiline");
-  li.setAttribute("style","text-align:center;");
-  li.setAttribute("id", elem_id);
-  li.innerHTML = token;
+    var li = document.createElement("li");
+    li.setAttribute("class","li-has-multiline");
+    li.setAttribute("style","text-align:center;");
+    li.setAttribute("id", elem_id);
+    li.innerHTML = token;
 
-  var span = document.createElement("div");
-  span.setAttribute("class","ui-li-sub-text li-text-sub");
-  span.innerHTML = "[ " + service + " ] " + id;
+    var span = document.createElement("div");
+    span.setAttribute("class","ui-li-sub-text li-text-sub");
+    span.innerHTML = "[ " + service + " ] " + id;
 
-  li.appendChild(span);
+    li.appendChild(span);
 
-  return li;
+    return li;
 }
 
 function init(result) {
-  keyItems = result;
+    keyItems = result;
 
-  var footer = document.getElementById("footer");
-  var progress = document.getElementById("progress");
+    var footer = document.getElementById("footer");
+    var progress = document.getElementById("progress");
 
-  if (result.example) {
-    var li= document.createElement("li");
-    var msg = document.createElement("div");
-    msg.setAttribute("class","ui-marquee ui-marquee-gradient");
-    msg.setAttribute("onclick", "tau.changePage('caution');");
-    msg.innerHTML = "auth_keyinfo.txt not found. You need create and put auth_keyinfo.txt to documents folder";
-    li.appendChild(msg);
-    tokenList.appendChild(li);
-    if (tau.support.shape.circle) {
-      tau.helper.SnapListMarqueeStyle.create(tokenList, { marqueeDelay: 1000, marqueeStyle: "endToEnd" });
+    if (result.example) {
+        var li= document.createElement("li");
+        var msg = document.createElement("div");
+        msg.setAttribute("class","ui-marquee ui-marquee-gradient");
+        msg.setAttribute("onclick", "tau.changePage('caution');");
+        msg.innerHTML = "auth_keyinfo.txt not found. You need create and put auth_keyinfo.txt to documents folder";
+        li.appendChild(msg);
+        tokenList.appendChild(li);
+        if (tau.support.shape.circle) {
+            tau.helper.SnapListMarqueeStyle.create(tokenList, { marqueeDelay: 1000, marqueeStyle: "endToEnd" });
+        }
+        tau.changePage("caution");
     }
-    tau.changePage("caution");
-  }
 
-  if (tau.support.shape.circle) {
-    footer.style.display="none";
-    progressBarWidget = new tau.widget.CircleProgressBar(progress, {size: "full"});
-    UpdateRemainTimeInner = UpdateRemainTimeCircle;
-    InitList();
-    snapListComponent = tau.widget.SnapListview(tokenList);
-    circle_helper(tau);
-  } else {
-    progress.style.display="none";
-    remainTime = document.getElementById("remainTime");
-    UpdateRemainTimeInner = UpdateRemainTimeRectangle;
-    InitList();
-  }
+    if (tau.support.shape.circle) {
+        footer.style.display="none";
+        progressBarWidget = new tau.widget.CircleProgressBar(progress, {size: "full"});
+        UpdateRemainTimeInner = UpdateRemainTimeCircle;
+        InitList();
+        snapListComponent = tau.widget.SnapListview(tokenList);
+        circle_helper(tau);
+    } else {
+        progress.style.display="none";
+        remainTime = document.getElementById("remainTime");
+        UpdateRemainTimeInner = UpdateRemainTimeRectangle;
+        InitList();
+    }
 
-  window.setInterval(Update,1000);
+    window.setInterval(Update,1000);
 }
 
 function Redraw() {
-  tokenList.innerHTML = "";
-  document.getElementById("Title").innerHTML=TITLE;
-  ReadItems(init);
+    tokenList.innerHTML = "";
+    document.getElementById("Title").innerHTML=TITLE;
+    ReadItems(init);
 }
--- js/store.js
+++ js/store.js
@@ -1,89 +1,86 @@
 function ReadError(next){
-  var keyItems=[];
+    var keyItems=[];
 
-  var keyItem = {};
-  keyItem.service = "EXAMPLE";
-  keyItem.id = "";
-  keyItem.key = base32tohex("JBSWY3DPEHPK3PXP");
-  keyItems.push(keyItem);
+    var keyItem = {};
+    keyItem.service = "EXAMPLE";
+    keyItem.id = "";
+    keyItem.key = base32tohex("JBSWY3DPEHPK3PXP");
+    keyItems.push(keyItem);
 
-  keyItem = {};
-  keyItem.service = "EXAMPLE";
-  keyItem.id = "";
-  keyItem.key = base32tohex("OFKLYERJNLGOIPTA");
-  keyItems.push(keyItem);
+    keyItem = {};
+    keyItem.service = "EXAMPLE";
+    keyItem.id = "";
+    keyItem.key = base32tohex("OFKLYERJNLGOIPTA");
+    keyItems.push(keyItem);
 
-  keyItems.example = true;
-  next(keyItems);
+    keyItems.example = true;
+    next(keyItems);
 }
 
 function moveAuthFile(folders, srcnames, next ){
-	var folder = folders.pop();
-	var srcname = srcnames.pop();
-	tizen.filesystem.resolve(folder + "/" + srcname,
-			function(file){
-				tizen.filesystem.resolve(folder,
-						function(path) {
-							path.moveTo(file.fullPath, "documents/auth_keyinfo.txt", true, next, next);
-						},
-						next,
-						"rw");
-			},
-			function(){
-				if(folders.length>0){
-					moveAuthFile(folders,srcnames, next);
-				} else {
-					next();
-				}
-			});
+    var folder = folders.pop();
+    var srcname = srcnames.pop();
+    tizen.filesystem.resolve(folder + "/" + srcname,
+        function(file){
+            tizen.filesystem.resolve(folder,
+            function(path) {
+                path.moveTo(file.fullPath, "documents/auth_keyinfo.txt", true, next, next);
+            }, next, "rw");
+        },
+    function(){
+        if(folders.length>0){
+            moveAuthFile(folders,srcnames, next);
+        } else {
+            next();
+        }
+    });
 }
 
 function ReadItems(next){
-	moveAuthFile(
-			["downloads","music"],
-			["auth_keyinfo.txt","auth_keyinfo.mp3"],
-		function() {
-		  tizen.filesystem.resolve(
-			      "documents/auth_keyinfo.txt", function(file) {
-			        file.readAsText(function(data) {
-			          var keyItems=[];
-			          var keys = data.split(/\r\n|\r|\n/);
-			          for(var i=0;i<keys.length;i++){
-			            var key = keys[i];
-			            var splited = key.split(":");
-			            if(splited.length >= 3){
-			              var keyItem = {};
-			              keyItem.service = splited[0];
-			              keyItem.id = splited[1];
-			              keyItem.key = base32tohex(splited[2].toUpperCase());
-			              if(splited.length === 4){
-			                keyItem.digits = splited[3];
-			              } else {
-			                keyItem.digits = 6;
-			              }
-			              keyItems.push(keyItem);
-			            }
-			          }
-			          next(keyItems);
-			        }, function() {ReadError(next);});
-			      }, function() { ReadError(next);});
-		}
-	);
+    moveAuthFile(
+        ["downloads","music"], ["auth_keyinfo.txt","auth_keyinfo.mp3"],
+        function() {
+            tizen.filesystem.resolve( "documents/auth_keyinfo.txt", 
+                function(file) {
+                    file.readAsText(function(data) {
+                        var keyItems=[];
+                        var keys = data.split(/\r\n|\r|\n/);
+                        for(var i=0;i<keys.length;i++){
+                            var key = keys[i];
+                            var splited = key.split(":");
+                            if(splited.length >= 3){
+                                var keyItem = {};
+                                keyItem.service = splited[0];
+                                keyItem.id = splited[1];
+                                keyItem.key = base32tohex(splited[2].toUpperCase());
+                                if(splited.length === 4){
+                                    keyItem.digits = splited[3];
+                                } else {
+                                    keyItem.digits = 6;
+                                }
+                                keyItems.push(keyItem);
+                            }
+                        }
+                        next(keyItems);
+                    }, function() {ReadError(next);});
+                }, function() { ReadError(next);});
+        }
+    );
 }
 
 function base32tohex (str) {
-  var base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
+    var base32chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ234567";
 
-  var bits = "";
-  for(var i = 0; i < str.length; i++) {
-    var val = base32chars.indexOf(str[i]);
-    bits += ("00000" + val.toString(2)).slice(-5);
-  }
+    var bits = "";
+    for(var i = 0; i < str.length; i++) {
+        var val = base32chars.indexOf(str[i]);
+        bits += ("00000" + val.toString(2)).slice(-5);
+    }
 
-  var hex = "";
-  for( i = 0; i + 4 <= bits.length; i+=4) {
-    var chunk = bits.substr(i, 4);
-    hex += parseInt(chunk, 2).toString(16) ;
-  }
-  return hex;
+    var hex = "";
+    for( i = 0; i + 4 <= bits.length; i+=4) {
+        var chunk = bits.substr(i, 4);
+        hex += parseInt(chunk, 2).toString(16) ;
+    }
+    return hex;
 }
